ee9ad4f1f93f26d98bcd5c8424407b5e12c8c079
Triyan1053 <61227305+Triyan1053@users.noreply.github.com>
2022-08-25 10:45:05 +0530

    make new help command

--- Helper/extensions/help.py
+++ Helper/extensions/help.py
@@ -1,103 +1,133 @@
-from itertools import starmap
-
 import discord
-from discord.ext import commands, menus
+import humanfriendly
+from discord.ext import commands
 
 from ..main import HelperBot
 from ..utils import Support
 
 
-class HelpPageSource(menus.ListPageSource):
-    def __init__(self, data, helpcommand, mode, cog_check: bool = False):
-        super().__init__(data, per_page=25)
+class Formatter:
+    def __init__(self, helpcommand: commands.MinimalHelpCommand) -> None:
+        self.ctx = helpcommand.context
         self.helpcommand = helpcommand
-        self.mode = mode
-        self.cog_check = cog_check
-        self.holder: list[discord.Embed] = []
 
-    def format_command_help(self, no, command):
-        prefix = self.helpcommand.context.prefix
-        name = command.qualified_name
-        signature = str(command.signature).lower().replace("=none", "")
-        docs = command.short_doc or "Command is not documented."
-        if signature:
-            return f"> ‣ **{prefix}{name}** `{signature}` - {docs}"
-        else:
-            return f"> ‣ **{prefix}{name}** - {docs}"
+    def __format_command_signature(self, command: commands.Command) -> str:
+        params = self.__format_param(command)
+        return f"{command.qualified_name}\n ```yaml\n{params}```"
 
-    async def format_page(self, menu, entries, num, curr_page):
-        iterator = starmap(self.format_command_help, enumerate(entries, start=1))
-        page_content = "\n".join(iterator)
-        new_line_and_pgno = "\n" + f"Page {curr_page} of {num}"
-        desc = ""
-        if self.cog_check:
-            cog = {k.lower(): k for k, v in menu.context.bot.cogs.items()}
-            cog = cog[self.mode.lower()]
-            cog_ = menu.context.bot.get_cog(cog)
-            desc = data if (data := cog_.description) else "No description found..."
+    def __format_param(self, param: commands.Command) -> str:
+        signature = self.helpcommand.get_command_signature(param).replace("=None", "")
+        return signature
 
+    @staticmethod
+    def __format_command_help(command: commands.Command) -> str:
+        return command.help or "No help provided."
+
+    @staticmethod
+    def __format_command_aliases(command: commands.Command) -> str:
+        return (
+            f"```yaml\nAliases: {', '.join(command.aliases)}```"
+            if command.aliases
+            else "No aliases."
+        )
+
+    @staticmethod
+    def __format_command_cooldown(command: commands.Command) -> str:
+        return (
+            f"Cooldown: {humanfriendly.format_timespan(command.cooldown.per)} per user."
+            if command.cooldown
+            else "No cooldown set."
+        )
+
+    @staticmethod
+    def __format_command_enabled(command: commands.Command) -> str:
+        return (
+            f"Enabled: {command.enabled}" if command.enabled else "Command is disabled."
+        )
+
+    @staticmethod
+    def __format_command_description(command: commands.Command) -> str:
+        return command.description or "No description provided."
+
+    def format_command(self, command: commands.Command) -> discord.Embed:
         embed = discord.Embed(
-            title=f"{self.mode.title()} Command"
-            if not self.cog_check
-            else f"{cog} Commands",
-            description=f"{desc}\n\n{page_content}",
+            title=self.__format_command_signature(command),
+            description=self.__format_command_help(command),
             color=0x2F3136,
         )
-        author = menu.context.author
-        embed.set_footer(
-            text=f"Requested by {author} {new_line_and_pgno}",
-            icon_url=author.display_avatar,
+        embed.add_field(
+            name="Aliases", value=self.__format_command_aliases(command), inline=True
         )
-        self.holder.append(embed)
+        embed.add_field(
+            name="Cooldown", value=self.__format_command_cooldown(command), inline=True
+        )
+        embed.add_field(
+            name="Enabled", value=self.__format_command_enabled(command), inline=True
+        )
+        embed.set_footer(
+            text=f"Requested by {self.ctx.author}",
+            icon_url=self.ctx.author.display_avatar,
+        )
         return embed
+
+    def format_pages(
+        self, pages: dict[str, list[commands.Command]], desc: list[str]
+    ) -> list[discord.Embed]:
+        embeds = []
+        for num, (name, allcommand) in enumerate(pages.items()):
+            embed = discord.Embed(title=name, description=desc[num], color=0x2F3136)
+            for command in allcommand:
+                embed.add_field(
+                    name=self.__format_command_signature(command),
+                    value=self.__format_command_help(command),
+                )
+            embed.set_footer(
+                text=f"Requested by {self.ctx.author} | Page {num + 1}/{len(pages)}",
+                icon_url=self.ctx.author.display_avatar,
+            )
+            embeds.append(embed)
+        return embeds
 
 
 class MyHelp(commands.MinimalHelpCommand):
     async def send_bot_help(self, mapping):
-        filtered = lambda i: self.filter_commands(i, sort=False)
-        category_wise = {
-            k: x
-            for k, v in self.context.bot.cogs.items()
-            if (x := await filtered(v.get_commands()))
-        }
-        formatting = lambda x, y, num, z: HelpPageSource(
-            x, self, y, cog_check=True
-        ).format_page(self, x, num, z)
-        category_formatter = [
-            await formatting(v, k, len(category_wise), i)
-            for i, (k, v) in enumerate(category_wise.items(), start=1)
+        category = {}
+        for k, v in self.context.bot.cogs.items():
+            if len((x := v.get_commands())) > 0:
+                category[k] = x
+        categorydesc = [
+            x if (x := i.description) else "No description provided."
+            for i in self.context.bot.cogs.values()
+            if len(i.get_commands()) > 0
         ]
-        await Support().paginate(category_formatter, self.context)
+        formatter = Formatter(self)
+        await Support().paginate(
+            formatter.format_pages(category, categorydesc), self.context
+        )
 
     async def send_cog_help(self, cog):
         if len(subcommands := cog.get_commands()) == 0:
             return await self.send_empty_cog_help(cog)
-        filtered = await self.filter_commands(subcommands, sort=True)
-        formatter = await HelpPageSource(
-            filtered, self, f"{cog.qualified_name}", cog_check=True
-        ).format_page(self, filtered, 1, 1)
-        await Support().paginate([formatter], self.context)
+        category = {cog.qualified_name: subcommands}
+        categorydesc = (
+            [cog.description] if cog.description else ["No description provided."]
+        )
+        formatter = Formatter(self)
+        await Support().paginate(
+            formatter.format_pages(category, categorydesc), self.context
+        )
 
     async def send_group_help(self, group):
-        # if len(subcommands := group.commands) == 0:
-        #     return await self.send_command_help(group)
-        # filtered = await self.filter_commands(subcommands, sort=True)
-        # filtered.insert(0, group)
-        # formatter = await HelpPageSource(filtered, self, f"{group.qualified_name}").format_page(self, filtered, 1, 1)
-        # await Support().paginate([formatter], self.context)
-        if len(subcommands := group.commands) == 0:
+        if len(subcommands := group.all_commands) == 0:
             return await self.send_command_help(group)
-        filtered = await self.filter_commands(subcommands, sort=True)
-        filtered.insert(0, group)
-        chunks = list(discord.utils.as_chunks(filtered, 6))
-        embeds = []
-        for chunk in chunks:
-            embeds.append(
-                await HelpPageSource(
-                    chunk, self, f"{group.qualified_name}"
-                ).format_page(self, chunk, len(chunks), len(embeds) + 1)
-            )
-        await Support().paginate(embeds, self.context)
+        allcommands = list(subcommands.values())
+        allcommands.insert(0, group)
+        category = {group.qualified_name: allcommands}
+        categorydesc = [group.help] if group.help else ["No description provided."]
+        formatter = Formatter(self)
+        await Support().paginate(
+            formatter.format_pages(category, categorydesc), self.context
+        )
 
     async def send_empty_cog_help(self, cog):
         embed = discord.Embed(
@@ -112,24 +142,8 @@ class MyHelp(commands.MinimalHelpCommand):
         await self.context.send(embed=embed)
 
     async def send_command_help(self, command):
-        embed = discord.Embed(
-            color=0x2F3136,
-            title=f"{command.qualified_name.title()} Command",
-        )
-        if command.description:
-            embed.description = f"{command.description}\n\n{command.help}"
-        else:
-            embed.description = command.help or "No help found..."
-        embed.add_field(
-            name="Usage",
-            value=f"```{self.get_command_signature(command).lower().replace('=none', '')}```",
-        )
-        if command.aliases:
-            embed.add_field(name="Aliases", value=f"```{', '.join(command.aliases)}```")
-        embed.set_footer(
-            text=f"Requested by {self.context.author}",
-            icon_url=self.context.author.display_avatar,
-        )
+        formatter = Formatter(self)
+        embed = formatter.format_command(command)
         await self.context.send(embed=embed)
 
 
